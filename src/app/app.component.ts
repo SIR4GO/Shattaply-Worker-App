bf6dd6053546e1395c42dca6fd7066e43df691f4
SIR4GO <e.m.ahmed@stud.fci-cu.edu.eg>
2019-06-06 11:44:40 +0200

    home page , menue

---
+++ src/app/app.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent  {
+  public appPages = [
+    {
+      title: 'الرائيسية',
+      url: '/home',
+      icon: 'home'
+    },
+    {
+      title: ' الطلبات الواردة',
+      url: '/list',
+      icon: 'build'
+    }
+    ,
+    {
+      title: 'الطلبات الحالية',
+      url: '/list',
+      icon: 'briefcase'
+    }
+    ,
+    {
+      title: ' من نحن',
+      url: '/list',
+      icon: 'md-contacts'
+    }
+    ,
+    {
+      title: ' سياسة الاستخدام',
+      url: '/list',
+      icon: 'bulb'
+    },
+    {
+      title: 'الأعدادت',
+      url: '/list',
+      icon: 'cog'
+    }
+  ];
+
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+}
