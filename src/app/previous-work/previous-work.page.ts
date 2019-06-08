9277e94f2d75629c128e12ca9c144bf85a5831fe
SIR4GO <e.m.ahmed@stud.fci-cu.edu.eg>
2019-06-08 12:59:30 +0200

    Accessing Native && Device Photo viewer

--- src/app/previous-work/previous-work.page.ts
+++ src/app/previous-work/previous-work.page.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , ViewChild } from '@angular/core';
+import {IonSlides} from '@ionic/angular';
+import { PhotoViewer } from '@ionic-native/photo-viewer/ngx';
+
+
 
 @Component({
   selector: 'app-previous-work',
   templateUrl: './previous-work.page.html',
   styleUrls: ['./previous-work.page.scss'],
 })
+
+
 export class PreviousWorkPage implements OnInit {
 
-  constructor() { }
+  @ViewChild(IonSlides) slides: IonSlides;
+
+  constructor( private photoViewer: PhotoViewer) { }
+
+  slideOpts = {
+    initialSlide: 1,
+    speed: 400,
+    autoplay: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
+  };
 
   ngOnInit() {
   }
+  getActiveIndex(){
+      this.slides.getActiveIndex().then(value => {
+          console.log(value);
+      });
+  }
 
+  showImage(img: HTMLElement) {
+    const imgSrc = img.getAttribute('src');
+    this.photoViewer.show(imgSrc, 'hello');
+  }
 }
